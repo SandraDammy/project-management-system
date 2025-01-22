ee08809dadd90e7b0e0c331298c51f44a1a95db1
Oluwadamilola S. Odujirin <oluwadamilolaod@HA-SHEM.com>
2025-01-22 15:49:08 +0100

    page creating

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,12 +5,13 @@ export default {
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    
   ],
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        lightGray: "var(--lightGray)",
+        almostBlack: "var(--almostBlack)",
       },
     },
   },
